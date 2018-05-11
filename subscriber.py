7fcfd11bdc822cb899dd948c7f317d7ea6d26299
Suyash Soni <suyash.soni248@gmail.com>
2018-05-11 18:07:56 +0530

    Added similuation image

--- subscriber.py
+++ subscriber.py
@@ -14,12 +14,15 @@ def fetch_and_process_msg(topic, msg_id):
         msg = MSG.get_msg(topic, msg_id)
         process_raw_msg(topic, msg)
         MSG.mark_msg_as_read(topic, msg_id, sub_id)
+    else:
+        print("Message has already been read!")
 
 def process_pending_msgs(*topics):
     for topic in topics:
         for topic_msg_key in MSG.get_msgs_for_topic(topic):
             topic_msg_key = topic_msg_key.decode()
             msg_id = MSG.extract_msg_id_from_topic_msg_key(topic_msg_key)
+            print("Pending message found on topic {}".format(topic))
             fetch_and_process_msg(topic, msg_id)
 
 def subscribe(port, topic):
@@ -39,7 +42,7 @@ def subscribe(port, topic):
 
     while True:
         topic_msg = sub.recv().decode()
-        print("New message...", topic_msg)
+        print("New message: ", topic_msg)
         for_topic = topic_msg[:topic_msg.find(' ')]
         msg_id = topic_msg[topic_msg.find(' ') + 1:]
         fetch_and_process_msg(for_topic, msg_id)
