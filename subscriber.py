6494aedc639b4c44c1c02025ba4f51e497a11d4f
Suyash Soni <suyash.soni248@gmail.com>
2018-05-12 00:01:56 +0530

    Dynamic subscribers

--- subscriber.py
+++ subscriber.py
@@ -12,8 +12,9 @@ def fetch_and_process_msg(topic, msg_id):
     global sub_id
     if not MSG.is_msg_read(topic, msg_id, sub_id):
         msg = MSG.get_msg(topic, msg_id)
-        process_raw_msg(topic, msg)
-        MSG.mark_msg_as_read(topic, msg_id, sub_id)
+        if msg is not None:
+            process_raw_msg(topic, msg)
+            MSG.mark_msg_as_read(topic, msg_id, sub_id)
     else:
         print("Message has already been read!")
 
@@ -25,20 +26,19 @@ def process_pending_msgs(*topics):
             print("Pending message found on topic {}".format(topic))
             fetch_and_process_msg(topic, msg_id)
 
-def subscribe(port, topic):
+def subscribe(port, topics=[]):
     context = zmq.Context()
     # sub = context.sub(zmq.SUB)
 
     sub = context.socket(zmq.SUB)
     print("Waiting for messages...")
     sub.connect("tcp://localhost:{}".format(port))
-    # for topic in topics:
-    topic = topic.strip()
 
-    process_pending_msgs(settings.GLOBAL_TOPIC, topic)
+    topics.extend(settings.GLOBAL_TOPICS)
+    for topic in topics:
+        sub.setsockopt(zmq.SUBSCRIBE, topic.strip().encode())
 
-    sub.setsockopt(zmq.SUBSCRIBE, topic.encode())
-    sub.setsockopt(zmq.SUBSCRIBE, settings.GLOBAL_TOPIC.encode())
+    process_pending_msgs(*topics)
 
     while True:
         topic_msg = sub.recv().decode()
@@ -50,5 +50,5 @@ def subscribe(port, topic):
 if __name__ == "__main__":
     sub_id = input("Enter subscriber id(alphanumeric): ")
     port = int(input("Enter port: "))
-    topic = input("Enter topic name to subscribe to: ")
-    subscribe(port, topic)
+    topics = input("Enter topic names(comma-separated) to subscribe to: ")
+    subscribe(port, topics.split(","))
